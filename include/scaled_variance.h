096d2f770abb0a63c5e50e47a73f2445f3949a9c
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2019-01-15 19:52:23 -0800

    Reorganize code: ScaledVarianceCalculator class
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- include/scaled_variance.h
+++ include/scaled_variance.h
@@ -6,13 +6,88 @@
 
 bool load_particle_types();
 
-std::pair<double, double> scaled_variance(
-    double T, double mub, double mus, double muq, bool quantum_statistics,
-    std::function<bool(const smash::ParticleType &)> type_in_the_box,
-    std::function<bool(const smash::ParticleType &)> type_of_interest,
-    bool energy_conservation, bool B_conservation, bool S_conservation,
-    bool Q_conservation);
+class ScaledVarianceCalculator {
+ public:
+  ScaledVarianceCalculator(const smash::ParticleTypePtrList& types_in_the_box,
+                           double T, double mub, double mus, double muq,
+                           bool energy_conservation,
+                           bool B_conservation,
+                           bool S_conservation,
+                           bool Q_conservation,
+                           bool quantum_statistics) :
+    T_ (T),
+    mub_ (mub),
+    mus_ (mus),
+    muq_ (muq),
+    energy_conservation_ (energy_conservation),
+    B_conservation_ (B_conservation),
+    S_conservation_ (S_conservation),
+    Q_conservation_ (Q_conservation),
+    quantum_statistics_ (quantum_statistics),
+    quantum_series_max_terms_(100),
+    quantum_series_rel_precision_(1e-12) {
+    all_types_in_the_box_.clear();
+    for (const smash::ParticleTypePtr t : types_in_the_box) {
+      all_types_in_the_box_.push_back(t);
+    }
+  }
+  void set_T (double T) { T_ = T; }
+  void set_mub (double mub) { mub_ = mub; }
+  void set_mus (double mus) { mus_ = mus; }
+  void set_muq (double muq) { muq_ = muq; }
+  void set_B_conservation (bool B_cons) { B_conservation_ = B_cons; }
+  void set_S_conservation (bool S_cons) { S_conservation_ = S_cons; }
+  void set_Q_conservation (bool Q_cons) { Q_conservation_ = Q_cons; }
+  void set_quantum_statistics (bool qs) { quantum_statistics_ = qs; }
 
-void initialize_random_number_generator();
+  /**
+   * Assumes that there is a gas of particles species defined
+   * by all_types_in_the_box_. This function computes fluctuations
+   * of the total number of particles belonging to a subset of species
+   * defined by type_of_interest function. Fluctuations depend on
+   * the ensemble and which conservation laws it respects. This is
+   * determined by variables energy_conservation_, B_conservation_,
+   * S_conservation_, and Q_conservation_.
+   *
+   * For reference see arxiv.org/pdf/0706.3290.pdf, Eq. (29-40).
+   * The approximations of this paper are only valid for large
+   * enough volume.
+   *
+   * \param[in] type_of_interest Function true for particle species, for
+   *            which the mean and the scaled variance are computed
+   * \param[out] mean density and scaled variance of type_of_interest species
+   */
+  std::pair<double, double> scaled_variance(
+    std::function<bool(const smash::ParticleTypePtr)> type_of_interest);
+
+  friend std::ostream& operator<< (std::ostream&,
+                                   const ScaledVarianceCalculator&);
+
+ private:
+  /// List of species included in the gas
+  smash::ParticleTypePtrList all_types_in_the_box_;
+  /// Temperature of the gas [GeV]
+  double T_;
+  /// Baryo-chemical potential of the gas [GeV]
+  double mub_;
+  /// Strangeness chemical potential of the gas [GeV]
+  double mus_;
+  /// Charge chemical potential of the gas [GeV]
+  double muq_;
+  /// If ensemble includes energy conservation
+  bool energy_conservation_;
+  /// If ensemble includes baryon number conservation
+  bool B_conservation_;
+  /// If ensemble includes strangeness conservation
+  bool S_conservation_;
+  /// If ensemble includes electric charge conservation
+  bool Q_conservation_;
+  /// If quantum statistics is included
+  bool quantum_statistics_;
+  /// Maximal number of terms in the series for quantum formulas
+  const unsigned int quantum_series_max_terms_;
+  /// Relative precision, at which quantum series summation stops
+  const double quantum_series_rel_precision_;
+};
 
 #endif // SCALED_VARIANCE_H
--- scaled_variance.cc
+++ scaled_variance.cc
@@ -4,8 +4,6 @@
 #include <fstream>
 #include <gsl/gsl_sf_bessel.h>
 
-#include "smash/hadgas_eos.h"
-
 /// Loads particle table from SMASH
 bool load_particle_types() {
   std::cout << "Loading SMASH particle types and decay modes" << std::endl;
@@ -38,58 +36,29 @@ bool load_particle_types() {
   return false;
 }
 
-/**
- * Returns expected scaled variance for a set of species defined by
- * type_of_interest function, in a Boltzmann gas of particles, where species
- * are set by type_in_the_box. For reference see arxiv.org/pdf/0706.3290.pdf,
- * Eq. (29-40). The approximations of this paper are only valid for large
- * enough volume.
- *
- * \param[in] T temperature of the gas
- * \param[in] mub baryo-chemical potential of the gas
- * \param[in] mus strangeness chemical potential of the gas
- * \param[in] type_in_the_box a function, that returns true for species, which
- *            are included in the gas
- * \param[in] type_of_interest a function that returns true for species, for
- *            which the scaled variance is computed
- * \param[in] energy_conservation if ensemble includes energy conservation
- * \param[in] energy_conservation if ensemble includes
- *            baryon number conservation
- * \param[in] energy_conservation if ensemble includes strangeness conservation
- * \param[in] energy_conservation if ensemble includes charge conservation
- * \param[out] mean density and scaled variance of type_of_interest species
- */
-std::pair<double, double> scaled_variance(
-    double T, double mub, double mus, double muq, bool quantum_statistics,
-    std::function<bool(const smash::ParticleType &)> type_in_the_box,
-    std::function<bool(const smash::ParticleType &)> type_of_interest,
-    bool energy_conservation, bool B_conservation, bool S_conservation,
-    bool Q_conservation) {
+std::pair<double, double> ScaledVarianceCalculator::scaled_variance(
+    std::function<bool(const smash::ParticleTypePtr)> type_of_interest) {
 
   constexpr unsigned int m = 5;
   Eigen::MatrixXd k2_tilde = Eigen::MatrixXd::Zero(m, m);
 
-  for (const smash::ParticleType &ptype : smash::ParticleType::list_all()) {
-    if (!type_in_the_box(ptype)) {
-      continue;
-    }
-    const double b = B_conservation ? ptype.baryon_number() : 0;
-    const double s = S_conservation ? ptype.strangeness() : 0;
-    const double q = Q_conservation ? ptype.charge() : 0;
-    const double z = ptype.mass() / T;
-    const double mu = mub * b + mus * s + muq *q;
-    const double mu_m_over_T = (mu - ptype.mass()) / T;
-    if (mu_m_over_T > 0 and quantum_statistics) {
-      std::cout << "Warning: quantum expressions for " << ptype.name() <<
+  for (const smash::ParticleTypePtr ptype : all_types_in_the_box_) {
+    const double b = B_conservation_ ? ptype->baryon_number() : 0;
+    const double s = S_conservation_ ? ptype->strangeness() : 0;
+    const double q = Q_conservation_ ? ptype->charge() : 0;
+    const double z = ptype->mass() / T_;
+    const double mu = mub_ * b + mus_ * s + muq_ * q;
+    const double mu_m_over_T = (mu - ptype->mass()) / T_;
+    if (mu_m_over_T > 0 and quantum_statistics_) {
+      std::cout << "Warning: quantum expressions for " << ptype->name() <<
                    " do not converge, m < chemical potential." << std::endl;
     }
-    const double factor = ptype.pdgcode().spin_degeneracy() * 4.0 * M_PI *
-                          std::pow(T / (2.0 * M_PI * smash::hbarc), 3);
+    const double factor = ptype->pdgcode().spin_degeneracy() * 4.0 * M_PI *
+                          std::pow(T_ / (2.0 * M_PI * smash::hbarc), 3);
     double EE = 0.0, EN = 0.0, NN = 0.0;
-    constexpr unsigned int maximum_terms = 50;
     // std::cout << "Computing matrix for " << ptype.name() << std::endl;
-    for (unsigned int k = 1; k < maximum_terms; k++) {
-      if (k > 1 and !quantum_statistics) {
+    for (unsigned int k = 1; k < quantum_series_max_terms_; k++) {
+      if (k > 1 and !quantum_statistics_) {
         break;
       }
       const double k1 = gsl_sf_bessel_Kn_scaled(1, z * k);
@@ -104,12 +73,12 @@ std::pair<double, double> scaled_variance(
       //           << ", EN_summand = " << EN_summand
       //           << ", EE_summand = " << EE_summand << std::endl;
       if (k > 1 and
-          EE_summand < EE * 1e-12 and
-          EN_summand < EN * 1e-12 and
-          NN_summand < NN * 1e-12) {
+          EE_summand < EE * quantum_series_rel_precision_ and
+          EN_summand < EN * quantum_series_rel_precision_ and
+          NN_summand < NN * quantum_series_rel_precision_) {
         break;
       }
-      if (k % 2 == 0 and ptype.pdgcode().is_baryon()) {
+      if (k % 2 == 0 and ptype->pdgcode().is_baryon()) {
         NN_summand = -NN_summand;
         EN_summand = -EN_summand;
         EE_summand = -EE_summand;
@@ -137,7 +106,7 @@ std::pair<double, double> scaled_variance(
     k2_tilde += k2_tilde_ptype;
   }
 
-  if (!energy_conservation) {
+  if (!energy_conservation_) {
     k2_tilde.row(1).setZero();
     k2_tilde.col(1).setZero();
   }
@@ -178,66 +147,69 @@ std::pair<double, double> scaled_variance(
   return std::make_pair(rho_type_interest, expected_scaled_variance);
 }
 
+std::ostream& operator<< (std::ostream& out,
+                          const ScaledVarianceCalculator &svc) {
+  out << "Scaled variance calculator" << std::endl;
+  out << "T [GeV] = " << svc.T_
+      << ", muB [GeV] = " << svc.mub_
+      << ", muS [GeV] = " << svc.mus_
+      << ", muQ [GeV] = " << svc.muq_ << std::endl;
+  out << "Quantum statistics = " << svc.quantum_statistics_ << std::endl;
+  out << "Included conservation laws: ";
+  if (svc.energy_conservation_) {
+    out << " energy;";
+  }
+  if (svc.B_conservation_) {
+    out << "  baryon number;";
+  }
+  if (svc.S_conservation_) {
+    out << " strangeness;";
+  }
+  if (svc.Q_conservation_) {
+    out << " electric charge;";
+  }
+  out << std::endl;
+  out << "Species in the box:" << std::endl;
+  for (const smash::ParticleTypePtr t : svc.all_types_in_the_box_) {
+    out << t->name() << " ";
+  }
+  out << std::endl;
+  return out;
+}
+
+
 int main() {
   load_particle_types();
-  smash::HadronGasEos eos(false);
-  const double V = 1000.0;
-  const double Energy = 500.0;
-  const double B_tot = 0.0, S_tot = 0.0;
-  constexpr bool quantum_statistics = false;
 
-  // The included hadron sorts are defined by smash::HadronGasEos::is_eos_particle
-  // std::array<double, 3> T_muB_muS = eos.solve_eos(Energy/V, B_tot/V, S_tot/V);
-  const double T = 0.2; // T_muB_muS[0];
-  const double mub = 0.0; // T_muB_muS[1];
-  const double mus = 0.0; // T_muB_muS[2];
-  const double muq = 0.0;
-  std::cout << "Energy [GeV] = " << Energy
-            << ", Volume [fm^3] = " << V
-            << ", B_tot = " << B_tot
-            << ", S_tot = " << S_tot << std::endl;
-  std::cout << "T [GeV] = " << T
-            << ", muB [GeV] = " << mub
-            << ", muS [GeV] = " << mus
-            << ", muQ = " << muq << std::endl;
-  std::cout << "Quantum statistics = " << quantum_statistics << std::endl;
-
-  std::vector<smash::PdgCode> pdgs_of_interest;
+  // Prepare the set of particle species in the box
+  std::vector<smash::ParticleTypePtr> hadrons_in_the_box;
   for (const smash::ParticleType &t : smash::ParticleType::list_all()) {
-    if (smash::HadronGasEos::is_eos_particle(t)) {
-      pdgs_of_interest.push_back(t.pdgcode());
+    if (t.is_hadron() && t.mass() < 1.0) {
+      hadrons_in_the_box.push_back(&t);
     }
   }
-  std::sort(pdgs_of_interest.begin(), pdgs_of_interest.end(),
-            [&](smash::PdgCode a, smash::PdgCode b) {
-              smash::ParticleTypePtr ta = &smash::ParticleType::find(a);
-              smash::ParticleTypePtr tb = &smash::ParticleType::find(b);
+  std::sort(hadrons_in_the_box.begin(), hadrons_in_the_box.end(),
+            [&](smash::ParticleTypePtr ta, smash::ParticleTypePtr tb) {
               return ta->mass() < tb->mass();
             });
-  for (const smash::PdgCode &pdg : pdgs_of_interest) {
-    const smash::ParticleType &ptype = smash::ParticleType::find(pdg);
-    const auto density_and_variance =
-        scaled_variance(T, mub, mus, muq, quantum_statistics,
-          [&](const smash::ParticleType &type_is_in_the_box) {
-            return smash::HadronGasEos::is_eos_particle(type_is_in_the_box);
-          },
-          [&](const smash::ParticleType &type_condition) {
-            return type_condition == ptype;
-          },
-          true, true, true, true);
-    std::cout << ptype.name() << " " << density_and_variance.first * V << " "
+
+  const double V = 1000.0;
+  ScaledVarianceCalculator svc(hadrons_in_the_box, 0.2, 0.0, 0.0, 0.0,
+                               true, true, true, true, true);
+  // svc.set_quantum_statistics(false);
+  std::cout << svc;
+
+  // Variance of each specie
+  for (const smash::ParticleTypePtr t : hadrons_in_the_box) {
+    const auto density_and_variance = svc.scaled_variance(
+          [&](const smash::ParticleTypePtr t0) { return t0 == t; });
+    std::cout << t->name() << " " << density_and_variance.first * V << " "
               << density_and_variance.second << std::endl;
   }
 
   // Variance of total number
-  const auto density_and_variance = scaled_variance(
-      T, mub, mus, muq, quantum_statistics,
-      [&](const smash::ParticleType &t) {
-        return smash::HadronGasEos::is_eos_particle(t);
-      },
-      [&](const smash::ParticleType &) { return true; }, true, true, true,
-      true);
-  std::cout << "Ntot"
-            << " " << density_and_variance.first * V << " "
+  const auto density_and_variance = svc.scaled_variance(
+      [&](const smash::ParticleTypePtr) { return true; });
+  std::cout << "Ntot " << density_and_variance.first * V << " "
             << density_and_variance.second << std::endl;
 }
